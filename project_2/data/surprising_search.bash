d48ea796e10bc60fd951d079a54c7c1596b81371
Per Magnus Veierland <pveierland@gmail.com>
2016-02-24 13:51:47 +0100

    P2: Search

--- project_2/data/surprising_search.bash
+++ project_2/data/surprising_search.bash
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # 30 seconds
-../program/surprising --adult_selection mixed --parent_selection tournament --group_size 25 --crossover_points 5 --mutation_rate 0.005 --population_size 500 --child_count 400 --S 32 --local --L 800 --generations 10000
+# 820 in 90 seconds
+#../program/surprising --adult_selection mixed --parent_selection tournament --group_size 25 --crossover_points 5 --mutation_rate 0.005 --population_size 500 --child_count 400 --S 32 --local --L 820 --generations 10000 --stop
 
-#../program/surprising --adult_selection mixed --parent_selection tournament --group_size 25 --crossover_points 5 --mutation_rate 0.005 --population_size 500 --child_count 400 --S 37 --global --L 90 --generations 10000
+# 92 in < 60 seconds
+# 95 in < 60 seconds
+#../program/surprising --adult_selection mixed --parent_selection tournament --group_size 25 --crossover_points 5 --mutation_rate 0.005 --population_size 500 --child_count 400 --S 37 --global --L 95 --generations 10000 --stop
+
+# 100 in 120 seconds
+../program/surprising --adult_selection over --parent_selection tournament --group_size 25 --crossover_points 10 --mutation_rate 0.007 --population_size 500 --child_count 600 --S 37 --global --L 100 --generations 10000 --stop
 
