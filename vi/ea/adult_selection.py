70fbcd5fd0524d88318546ee598fb7dc7dc47727
Per Magnus Veierland <pveierland@gmail.com>
2016-03-10 01:49:54 +0100

    P3: EA WIP

--- vi/ea/adult_selection.py
+++ vi/ea/adult_selection.py
@@ -1,26 +1,11 @@
-def fill_population(
-    system, 
-    past_generation, next_generation, population_target_size, system)
+def fill_population(system, past_generation, next_generation, fill_size):
+    system.parent_selection_function.register_population(past_generation)
 
-    parent_selection_function.register_population(past_generation)
 
-    generate_children = True
 
-    while generate_children:
-        children_genotypes = reproduction_function(parent_selection_function)
-
-        for child_genotype in children_genotypes:
-            child_phenotype = development_function(child_genotype)
-            child_fitness   = fitness_function(child_phenotype)
-
-            if child_phenotype and child_genotype:
-                child_individual = Individual(
-                    child_genotype, child_phenotype, child_fitness)
-                next_generation.append(child_individual)
-
-                if len(next_generation) == population_limit:
-                    generate_children = False
-                    break
+    while len(next_generation) < fill_size:
+        children = child_generation_function()
+        next_generation.extend(children[:len(next_generation) - fill_size])
 
 class full_generational_replacement(object):
     def __call__(self,
@@ -47,12 +32,7 @@ class generational_mixing(object):
     def __init__(self, num_children):
         self.num_children = num_children
 
-    def __call__(self,
-                 population,
-                 parent_selection_function,
-                 reproduction_function,
-                 development_function,
-                 fitness_function):
+    def __call__(self, system):
 
         population_size = len(population)
         num_competitors = population_size + self.num_children
@@ -71,26 +51,16 @@ class generational_mixing(object):
         return next_generation
 
 class overproduction(object):
-    def __init__(self, num_children):
+    def __init__(self, num_children)
         self.num_children = num_children
 
-    def __call__(self,
-                 population,
-                 reproduction_function,
-                 development_function,
-                 fitness_function):
-
+    def __call__(self, system, population):
         population_size = len(population)
         next_generation = []
 
-        fill_population(population,
-                        next_generation,
-                        self.num_children,
-                        reproduction_function,
-                        development_function,
-                        fitness_function):
+        fill_population(population, next_generation, self.num_children, system)
 
-        next_generation.sort(key=operator.methodcaller('fitness'), reverse=True)
+        next_generation.sort(key=operator.attrgetter('fitness'), reverse=True)
         next_generation = next_generation[:population_size]
 
         return next_generation
---
+++ vi/ea/reproduction.py
@@ -0,0 +1,22 @@
+import copy
+
+class sexual(object):
+    def __init__(self, crossover_function, mutation_function):
+        self.crossover_function = crossover_function
+        self.mutation_function  = mutation_function
+
+    def __call__(self, system):
+        parent_a = system.parent_selection_function()
+        parent_b = system.parent_selection_function()
+
+        child_a_genotype = copy.copy(parent_a.genotype)
+        child_b_genotype = copy.copy(parent_b.genotype)
+
+        child_a_genotype, child_b_genotype =
+            self.crossover_function(child_a_genotype, child_b_genotype)
+
+        child_a_genotype = self.mutation_function(child_a_genotype)
+        child_b_genotype = self.mutation_function(child_b_genotype)
+
+        return [child_a_genotype, child_b_genotype]
+
