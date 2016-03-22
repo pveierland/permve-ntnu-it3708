250565f38623a20f4406290f073e45bddc07b43c
Per Magnus Veierland <pveierland@gmail.com>
2016-03-22 15:51:17 +0100

    P3: vi-ea fix

---
+++ vi-ea/vi-ea.js
@@ -0,0 +1,13 @@
+import * as adult_selection from 'src/adult_selection';
+export { adult_selection };
+
+import * as fixed_bit_vector from 'src/fixed_bit_vector';
+export { fixed_bit_vector };
+
+import * as reproduction from 'src/reproduction';
+export { reproduction };
+
+import * as parent_selection from 'src/parent_selection';
+export { parent_selection };
+
+export { Individual, System } from 'src/system';
