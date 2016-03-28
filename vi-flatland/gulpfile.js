128eee64e23a80d2b39333ee3998015320d80a88
Per Magnus Veierland <pveierland@gmail.com>
2016-03-29 01:26:43 +0200

    P3: vi-flatland added deploy uglify task

--- vi-flatland/gulpfile.js
+++ vi-flatland/gulpfile.js
@@ -1,20 +1,34 @@
-var babelify    = require('babelify');
-var browserify  = require('browserify');
-var gulp        = require('gulp');
-var gulp_util   = require('gulp-util');
-var source      = require('vinyl-source-stream');
+var babelify   = require('babelify');
+var browserify = require('browserify');
+var buffer     = require('vinyl-buffer');
+var gulp       = require('gulp');
+var source     = require('vinyl-source-stream');
+var uglify     = require('gulp-uglify');
+var util       = require('gulp-util');
 
 gulp.task('es6', function() {
     browserify({
-        entries: 'src/vi-flatland.js',
-        debug: true,
+        entries:    'src/vi-flatland.js',
+        debug:      true,
         standalone: 'flatland'
     })
     .transform(babelify, {presets: ['es2015']})
-    .on('error', gulp_util.log)
     .bundle()
-    .on('error', gulp_util.log)
     .pipe(source('vi-flatland.js'))
+    .pipe(gulp.dest(''));
+});
+
+gulp.task('deploy', function() {
+    browserify({
+        entries:    'src/vi-flatland.js',
+        debug:      false,
+        standalone: 'flatland'
+    })
+    .transform(babelify, {presets: ['es2015']})
+    .bundle()
+    .pipe(source('vi-flatland.js'))
+    .pipe(buffer())
+    .pipe(uglify())
     .pipe(gulp.dest(''));
 });
 
