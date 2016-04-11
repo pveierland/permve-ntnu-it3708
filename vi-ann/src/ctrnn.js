d37e538d85f5d4ac8e883fda781fa8d85a0e2fc4
Per Magnus Veierland <pveierland@gmail.com>
2016-04-11 02:22:28 +0200

    P4: vi-ann added ctrnn

---
+++ vi-ann/src/ctrnn.js
@@ -0,0 +1,64 @@
+export class Network
+{
+    constructor(inputs = [], hidden = [], outputs = [])
+    {
+        this.inputs  = inputs;
+        this.hidden  = hidden;
+        this.outputs = outputs;
+
+        this.nodes   = hidden.concat(outputs);
+    }
+
+    evaluate(inputs)
+    {
+        for (let i = 0; i < inputs.length; i++)
+        {
+            this.inputs[i].value = inputs[i];
+        }
+
+        for (const node of this.nodes)
+        {
+            node.step();
+        }
+
+        for (const node of this.nodes)
+        {
+            node.update();
+        }
+
+        return this.outputs.map(output => output.value);
+    }
+}
+
+export class Node
+{
+    constructor(constants, activationFunction, inputs = [], weights = [])
+    {
+        this.constants          = constants;
+        this.activationFunction = activationFunction;
+        this.inputs             = inputs;
+        this.weights            = weights;
+
+        this.state = 0;
+        this.value = 0;
+    }
+
+    step()
+    {
+        let sum = 0;
+
+        for (let i = 0; i < this.inputs.length; i++)
+        {
+            sum += this.weights[i] * this.inputs[i].value;
+        }
+
+        const derivative = (-this.state + sum + this.constants.bias) / this.constants.tau;
+
+        this.state = this.state + derivative;
+    }
+
+    update()
+    {
+        this.value = this.activationFunction(this.constants.gain * this.state);
+    }
+}
--- vi-ann/vi-ann.js
+++ vi-ann/vi-ann.js
@@ -1,3 +1,4 @@
 import * as activation from './src/activation';
+import * as ctrnn from './src/ctrnn';
 import * as feedforward from './src/feedforward';
-export { activation, feedforward };
+export { activation, ctrnn, feedforward };
