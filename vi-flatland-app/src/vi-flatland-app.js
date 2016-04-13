06022a9ae7b5f2920e3e4b97fcb8c745432418f4
Per Magnus Veierland <pveierland@gmail.com>
2016-04-14 01:44:27 +0200

    P3: vi-flatland-app cleanup++

--- vi-flatland-app/src/vi-flatland-app.js
+++ vi-flatland-app/src/vi-flatland-app.js
@@ -22,96 +22,6 @@ export const WorldEntity = Object.freeze(
     void: 0, food: 1, poison: 2
 });
 
-function clearGridNeighbors(cells, x, y, width, height)
-{
-    if (x > 0)
-    {
-        cells[y * width + x - 1] = 0;
-    }
-    if (x < width - 1)
-    {
-        cells[y * width + x + 1] = 0;
-    }
-    if (y > 0)
-    {
-        cells[(y - 1) * width + x] = 0;
-    }
-    if (y < height - 1)
-    {
-        cells[(y + 1) * width + x] = 0;
-    }
-};
-
-function computeTargetCell(world, heading)
-{
-    switch (heading)
-    {
-        case Heading.up:
-        {
-            return { x: world.agent.x, y: (world.agent.y - 1 + world.height) % world.height };
-        }
-        case Heading.down:
-        {
-            return { x: world.agent.x, y: (world.agent.y + 1) % world.height };
-        }
-        case Heading.left:
-        {
-            return { x: (world.agent.x - 1 + world.width) % world.width, y: world.agent.y };
-        }
-        case Heading.right:
-        {
-            return { x: (world.agent.x + 1) % world.width, y: world.agent.y };
-        }
-    }
-};
-
-function getNeighborFromHeading(world, heading)
-{
-    const target = computeTargetCell(world, heading);
-    return world.cells[target.y * world.width + target.x];
-};
-
-export function generateRandomWorld(options)
-{
-    const cellCount = options.width * options.height;
-    const foodCount = Math.round(
-        options.foodProbability * cellCount);
-    const poisonCount = Math.round(
-        options.poisonProbability * (cellCount - foodCount));
-
-    let cells = new Array(cellCount).fill(WorldEntity.void);
-
-    let availableCells = utility.shuffle(cells.map((cv, i) => i));
-    const foodCells    = availableCells.splice(0, foodCount);
-    const poisonCells  = availableCells.splice(0, poisonCount);
-    const agentCell    = availableCells.splice(0, 1)[0];
-
-    for (let foodCell of foodCells)
-    {
-        cells[foodCell] = WorldEntity.food;
-    }
-
-    for (let poisonCell of poisonCells)
-    {
-        cells[poisonCell] = WorldEntity.poison;
-    }
-
-    return {
-        cells:             cells,
-        width:             options.width,
-        height:            options.height,
-        foodProbability:   options.foodProbability,
-        poisonProbability: options.poisonProbability,
-        foodCount:         foodCount,
-        poisonCount:       poisonCount,
-        agent: {
-            x:       agentCell % options.width,
-            y:       Math.floor(agentCell / options.width),
-            heading: math.randomInt(Heading.up, Heading.right + 1)
-        }
-    }
-}
-
 export function buildGameModelFromWorldModel(world)
 {
     const gridWidth    = 2 * world.width - 1;
@@ -189,7 +99,119 @@ export function buildGameModelFromWorldModel(world)
     };
 }
 
-export function evaluateRun(world, agent, timeSteps)
+export function createAgent(network)
+{
+    return {
+        act: function(world)
+        {
+            const leftHeading  = (world.agent.heading - 1 + 4) % 4;
+            const rightHeading = (world.agent.heading + 1) % 4;
+
+            const leftValue    = getNeighborFromHeading(world, leftHeading);
+            const forwardValue = getNeighborFromHeading(world, world.agent.heading);
+            const rightValue   = getNeighborFromHeading(world, rightHeading);
+
+            const inputs = [
+                forwardValue === WorldEntity.food   ? 1 : 0,
+                leftValue    === WorldEntity.food   ? 1 : 0,
+                rightValue   === WorldEntity.food   ? 1 : 0,
+                forwardValue === WorldEntity.poison ? 1 : 0,
+                leftValue    === WorldEntity.poison ? 1 : 0,
+                rightValue   === WorldEntity.poison ? 1 : 0 ];
+
+            const outputs = network.evaluate(inputs);
+
+            // Pick best action if one exists; otherwise do nothing.
+
+            if (outputs[0] > outputs[1] && outputs[0] > outputs[2])
+            {
+                return Action.moveForward;
+            }
+            else if (outputs[1] > outputs[0] && outputs[1] > outputs[2])
+            {
+                return Action.moveLeft;
+            }
+            else if (outputs[2] > outputs[0] && outputs[2] > outputs[1])
+            {
+                return Action.moveRight;
+            }
+            else
+            {
+                return Action.forward;//Action.stay;
+            }
+        }
+    };
+}
+
+export function createDevelopmentStrategy(options)
+{
+    return {
+        develop: function(genotype)
+        {
+            let offset = 0, hiddenLayer, outputLayer;
+
+            [hiddenLayer, offset] = evaluateLayer(
+                genotype, offset, options, options.inputLayer, options.hiddenLayer);
+
+            [outputLayer, offset] = evaluateLayer(
+                genotype, offset, options, options.hiddenLayer, options.outputLayer);
+
+            return new ann.feedforward.Network([hiddenLayer, outputLayer]);
+        }
+    };
+}
+
+export function createFitnessEvaluationStrategy(fitnessExpression, worlds, timeSteps)
+{
+    let totalFoodCount = 0, totalPoisonCount = 0;
+
+    for (const world of worlds)
+    {
+        totalFoodCount   += world.foodCount;
+        totalPoisonCount += world.poisonCount;
+    }
+
+    fitnessExpression = math.compile(fitnessExpression);
+
+    return {
+        evaluate: function(system, genotype, phenotype)
+        {
+            const agent       = createAgent(phenotype);
+            const evaluations = worlds.map(
+                world => evaluateAgent(world, agent, timeSteps));
+
+            let totalFoodEaten = 0, totalPoisonEaten = 0;
+
+            for (const evaluation of evaluations)
+            {
+                totalFoodEaten   += evaluation.foodEaten;
+                totalPoisonEaten += evaluation.poisonEaten;
+            }
+
+            return fitnessExpression.eval(
+            {
+                foodEaten:   totalFoodEaten,
+                poisonEaten: totalPoisonEaten,
+                foodCount:   totalFoodCount,
+                poisonCount: totalPoisonCount
+            });
+        }
+    };
+}
+
+export function createGenotypeCreator(options)
+{
+    const hiddenLayerBits = (options.hiddenLayer.size
+        * (options.hiddenLayer.bias.bits + options.inputLayer.size
+            * (options.hiddenLayer.weights.bits + options.weightSwitchBits)));
+    const outputLayerBits = (options.outputLayer.size
+        * (options.outputLayer.bias.bits + options.hiddenLayer.size
+            * (options.outputLayer.weights.bits + options.weightSwitchBits)));
+
+    return new ea.fixedBitVector.Creator(hiddenLayerBits + outputLayerBits);
+}
+
+export function evaluateAgent(world, agent, timeSteps)
 {
     world = JSON.parse(JSON.stringify(world));
 
@@ -271,78 +293,16 @@ export function evaluateRun(world, agent, timeSteps)
     return { foodEaten: foodEaten, poisonEaten: poisonEaten, moves: moves };
 }
 
-//const biasBits = 4;
-//
-//function setBias(bias, genotype, inputNodes, layerNodes, genotypeLayerOffset)
-//{
-//    for (let node = 0; node < layerNodes; node++)
-//    {
-//        const offset     = genotypeLayerOffset + node * (2 * inputNodes + biasBits) + 2 * inputNodes;
-//        const isNegative = genotype[offset + 3];
-//        const value      = (genotype[offset + 0] | 0) + (genotype[offset + 1] | 0) * 2 + (genotype[offset + 2] | 0) * 4;
-//
-//        bias[node] = isNegative ? value - 8 : value;
-//    }
-//}
-//
-//function setWeights(weights, genotype, inputNodes, layerNodes, genotypeOffset)
-//{
-//    for (let node = 0; node < layerNodes; node++)
-//    {
-//        for (let inputNode = 0; inputNode < inputNodes; inputNode++)
-//        {
-//            const offset = genotypeOffset + node * (2 * inputNodes + biasBits) + 2 * inputNode;
-//            weights[node * inputNodes + inputNode] = (genotype[offset] | 0) * (genotype[offset + 1] ? 1 : -1);
-//        }
-//    }
-//}
-
-//let stepFunction = function(stepValue)
-//{
-//    return function(input)
-//    {
-//        return input >= stepValue ? 1 : 0;
-//    }
-//}
-//
-//export function genotypeDevelopmentStrategy(genotype)
-//{
-//    const inputNodes  = 6;
-//    const hiddenNodes = 6;
-//    const outputNodes = 3;
-//
-//    let hiddenLayerBias    = new Array(hiddenNodes);
-//    let hiddenLayerWeights = new Array(inputNodes * hiddenNodes);
-//
-//    setWeights(hiddenLayerWeights, genotype, inputNodes, hiddenNodes, 0);
-//    setBias(hiddenLayerBias, genotype, inputNodes, hiddenNodes, 0);
-//
-//    let outputLayerBias = new Array(outputNodes);
-//    let outputLayerWeights = new Array(hiddenNodes * outputNodes);
-//
-//    setWeights(outputLayerWeights, genotype, hiddenNodes, outputNodes, 2 * inputNodes * hiddenNodes + biasBits * hiddenNodes);
-//    setBias(outputLayerBias, genotype, hiddenNodes, outputNodes, 2 * inputNodes * hiddenNodes + biasBits * hiddenNodes);
-//
-//    return new ann.feedforward.Network([
-//        new ann.feedforward.Layer(hiddenLayerBias, hiddenLayerWeights, stepFunction(1)),
-//        new ann.feedforward.Layer(outputLayerBias, outputLayerWeights, stepFunction(1))]);
-//}
-
-function evaluateWeight(genotype, offset, config, weightConfig)
+function evaluateGenotypeParameter(genotype, offset, config, grayCoding)
 {
-    if (config.weightSwitchBits && !genotype[offset])
-    {
-        return 0;
-    }
-
     let value = 0;
 
-    for (let i = 0; i < weightConfig.bits; i++)
+    for (let i = 0; i < config.bits; i++)
     {
-        value |= genotype[offset + config.weightSwitchBits + i] ? 2 << i : 0;
+        value |= genotype[offset + i] ? 2 << i : 0;
     }
 
-    if (config.grayCoding)
+    if (grayCoding)
     {
         let decoded = value;
 
@@ -354,149 +314,130 @@ function evaluateWeight(genotype, offset, config, weightConfig)
         value = decoded;
     }
 
-    value = (weightConfig.inclusiveRangeStart
-          + (value / ((2 << weightConfig.bits) - 1))
-          * (weightConfig.inclusiveRangeEnd - weightConfig.inclusiveRangeStart));
+    value = (config.inclusiveRangeStart
+          + (value / ((2 << config.bits) - 1))
+          * (config.inclusiveRangeEnd - config.inclusiveRangeStart));
 
-    return value;
+    return [value, offset + config.bits];
 }
 
 function evaluateLayer(genotype, offset, config, inputLayerConfig, layerConfig)
 {
-    let bias    = new Array(layerConfig.size);
-    let weights = new Array(layerConfig.size * inputLayerConfig.size);
+    const bias    = new Array(layerConfig.size);
+    const weights = new Array(layerConfig.size * inputLayerConfig.size);
 
     for (let layerNode = 0; layerNode < layerConfig.size; layerNode++)
     {
-        bias[layerNode] = evaluateWeight(
+        [bias[layerNode], offset] = evaluateGenotypeParameter(
             genotype,
             offset,
-            config,
-            layerConfig.bias);
-
-        offset += layerConfig.bias.bits;
+            layerConfig.bias,
+            config.grayCoding);
 
         for (let inputNode = 0; inputNode < inputLayerConfig.size; inputNode++)
         {
-            weights[layerNode * inputLayerConfig.size + inputNode] = evaluateWeight(
-                genotype, offset, config, layerConfig.weights);
+            const weightEnabled = !config.weightSwitchBits || genotype[offset++];
 
-            offset += layerConfig.weights.bits;
+            let weight;
+            [weight, offset] = evaluateGenotypeParameter(
+                genotype, offset, layerConfig.weights, config.grayCoding);
+
+            weights[layerNode * inputLayerConfig.size + inputNode] = weightEnabled * weight;
         }
+    }
+
+    const layer = new ann.feedforward.Layer(
+        bias, weights, ann.activation[layerConfig.activation]);
+
+    return [layer, offset];
+}
+
+export function generateRandomWorld(options)
+{
+    const cellCount = options.width * options.height;
+    const foodCount = Math.round(
+        options.foodProbability * cellCount);
+    const poisonCount = Math.round(
+        options.poisonProbability * (cellCount - foodCount));
+
+    let cells = new Array(cellCount).fill(WorldEntity.void);
+
+    let availableCells = utility.shuffle(cells.map((cv, i) => i));
+    const foodCells    = availableCells.splice(0, foodCount);
+    const poisonCells  = availableCells.splice(0, poisonCount);
+    const agentCell    = availableCells.splice(0, 1)[0];
+
+    for (let foodCell of foodCells)
+    {
+        cells[foodCell] = WorldEntity.food;
+    }
+
+    for (let poisonCell of poisonCells)
+    {
+        cells[poisonCell] = WorldEntity.poison;
     }
 
     return {
-        layer:  new ann.feedforward.Layer(
-            bias, weights, ann.activation[layerConfig.activation]),
-        offset: offset
-    };
-}
-
-export function createGenotypeCreator(options)
-{
-    const hiddenLayerBits = (options.hiddenLayer.size
-        * (options.hiddenLayer.bias.bits + options.inputLayer.size
-            * (options.hiddenLayer.weights.bits + options.weightSwitchBits)));
-    const outputLayerBits = (options.outputLayer.size
-        * (options.outputLayer.bias.bits + options.hiddenLayer.size
-            * (options.outputLayer.weights.bits + options.weightSwitchBits)));
-
-    console.log('GENOTYPE SIZE = ' + (hiddenLayerBits + outputLayerBits));
-
-    return new ea.fixedBitVector.Creator(hiddenLayerBits + outputLayerBits);
-}
-
-export function createDevelopmentFunction(options)
-{
-    return function(genotype)
-    {
-        const hidden = evaluateLayer(
-            genotype, 0, options, options.inputLayer, options.hiddenLayer);
-
-        const output = evaluateLayer(
-            genotype, hidden.offset, options, options.hiddenLayer, options.outputLayer);
-
-        return new ann.feedforward.Network([hidden.layer, output.layer]);
-    };
-}
-
-export function createFitnessFunction(fitnessExpression, worlds, timeSteps)
-{
-    let totalFoodCount = 0, totalPoisonCount = 0;
-
-    for (const world of worlds)
-    {
-        totalFoodCount   += world.foodCount;
-        totalPoisonCount += world.poisonCount;
-    }
-
-    return function(phenotype)
-    {
-        const agent       = new Agent(phenotype);
-        const evaluations = worlds.map(
-            world => evaluateRun(world, agent, timeSteps));
-
-        let totalFoodEaten = 0, totalPoisonEaten = 0;
-
-        for (const evaluation of evaluations)
-        {
-            totalFoodEaten   += evaluation.foodEaten;
-            totalPoisonEaten += evaluation.poisonEaten;
-        }
-
-        return fitnessExpression.eval(
-        {
-            foodEaten:   totalFoodEaten,
-            poisonEaten: totalPoisonEaten,
-            foodCount:   totalFoodCount,
-            poisonCount: totalPoisonCount
-        });
-    };
-}
-
-export class Agent
-{
-    constructor(network)
-    {
-        this.network = network;
-    }
-
-    act(world)
-    {
-        const leftHeading  = (world.agent.heading - 1 + 4) % 4;
-        const rightHeading = (world.agent.heading + 1) % 4;
-
-        const leftValue    = getNeighborFromHeading(world, leftHeading);
-        const forwardValue = getNeighborFromHeading(world, world.agent.heading);
-        const rightValue   = getNeighborFromHeading(world, rightHeading);
-
-        const inputs = [
-            forwardValue === WorldEntity.food   ? 1 : 0,
-            leftValue    === WorldEntity.food   ? 1 : 0,
-            rightValue   === WorldEntity.food   ? 1 : 0,
-            forwardValue === WorldEntity.poison ? 1 : 0,
-            leftValue    === WorldEntity.poison ? 1 : 0,
-            rightValue   === WorldEntity.poison ? 1 : 0 ];
-
-        const outputs = this.network.evaluate(inputs);
-
-        if (outputs[0] > outputs[1] && outputs[0] > outputs[2])
-        {
-            return Action.moveForward;
-        }
-        else if (outputs[1] > outputs[0] && outputs[1] > outputs[2])
-        {
-            return Action.moveLeft;
-        }
-        else if (outputs[2] > outputs[0] && outputs[2] > outputs[1])
-        {
-            return Action.moveRight;
-        }
-        else
-        {
-            //return Action.moveForward;
-            return Action.stay;
+        cells:             cells,
+        width:             options.width,
+        height:            options.height,
+        foodProbability:   options.foodProbability,
+        poisonProbability: options.poisonProbability,
+        foodCount:         foodCount,
+        poisonCount:       poisonCount,
+        agent: {
+            x:       agentCell % options.width,
+            y:       Math.floor(agentCell / options.width),
+            heading: math.randomInt(Heading.up, Heading.right + 1)
         }
     }
 }
 
+function clearGridNeighbors(cells, x, y, width, height)
+{
+    if (x > 0)
+    {
+        cells[y * width + x - 1] = 0;
+    }
+    if (x < width - 1)
+    {
+        cells[y * width + x + 1] = 0;
+    }
+    if (y > 0)
+    {
+        cells[(y - 1) * width + x] = 0;
+    }
+    if (y < height - 1)
+    {
+        cells[(y + 1) * width + x] = 0;
+    }
+};
+
+function computeTargetCell(world, heading)
+{
+    switch (heading)
+    {
+        case Heading.up:
+        {
+            return { x: world.agent.x, y: (world.agent.y - 1 + world.height) % world.height };
+        }
+        case Heading.down:
+        {
+            return { x: world.agent.x, y: (world.agent.y + 1) % world.height };
+        }
+        case Heading.left:
+        {
+            return { x: (world.agent.x - 1 + world.width) % world.width, y: world.agent.y };
+        }
+        case Heading.right:
+        {
+            return { x: (world.agent.x + 1) % world.width, y: world.agent.y };
+        }
+    }
+};
+
+function getNeighborFromHeading(world, heading)
+{
+    const target = computeTargetCell(world, heading);
+    return world.cells[target.y * world.width + target.x];
+};
