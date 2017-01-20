6549fea65c912489801b37ea072f274b85c5b9c8
Per Magnus Veierland <pveierland@gmail.com>
2017-01-20 19:58:47 +0100

    P1-2017: Completed assignment

--- project_1_2017/data/generate-learning-curves.bash
+++ project_1_2017/data/generate-learning-curves.bash
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-training_rounds=25
+training_rounds=30
 training_round_repetitions=30
 training_round_size=100
 
 parameters="--learning_rate 0.01 --train --training_rounds ${training_rounds} --training_round_repetitions ${training_round_repetitions} --training_round_size ${training_round_size}"
 
-../program/flatland.py --agent supervised --sensor_range 1 ${parameters} > learning-curve-supervised-1-sensor-range-${training_rounds}-training-rounds-${training_round_size}-size-${training_round_repetitions}-repetitions.txt
+../program/flatland.py --agent supervised --sensor_range 1 ${parameters} --baseline_take_food_near_wall --baseline_prefer_right > learning-curve-supervised-1-sensor-range-${training_rounds}-training-rounds-${training_round_size}-size-${training_round_repetitions}-repetitions.txt
 
 ../program/flatland.py --agent reinforcement --sensor_range 1 ${parameters} > learning-curve-reinforcement-1-sensor-range-${training_rounds}-training-rounds-${training_round_size}-size-${training_round_repetitions}-repetitions.txt
 
