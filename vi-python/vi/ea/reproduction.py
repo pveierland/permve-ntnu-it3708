eac23fbc4b26fe3d8b525d60ab3159770b436089
Per Magnus Veierland <pveierland@gmail.com>
2017-02-11 21:49:52 +0100

    P2-2017: Sexual reproduction crossover+mutation rates

--- vi-python/vi/ea/reproduction.py
+++ vi-python/vi/ea/reproduction.py
@@ -1,8 +1,11 @@
 import copy
+import random
 
 class Sexual(object):
-    def __init__(self, crossover_function, mutation_function):
+    def __init__(self, crossover_rate, crossover_function, mutation_rate, mutation_function):
+        self.crossover_rate     = crossover_rate
         self.crossover_function = crossover_function
+        self.mutation_rate      = mutation_rate
         self.mutation_function  = mutation_function
 
     def __call__(self, parent_selector):
@@ -12,11 +15,15 @@ class Sexual(object):
         child_a_genotype = copy.deepcopy(parent_a.genotype)
         child_b_genotype = copy.deepcopy(parent_b.genotype)
 
-        child_a_genotype, child_b_genotype = \
-            self.crossover_function(child_a_genotype, child_b_genotype)
+        if random.random() < self.crossover_rate:
+            child_a_genotype, child_b_genotype = \
+                self.crossover_function(child_a_genotype, child_b_genotype)
 
-        child_a_genotype = self.mutation_function(child_a_genotype)
-        child_b_genotype = self.mutation_function(child_b_genotype)
+        if self.mutation_function:
+            if random.random() < self.mutation_rate:
+                child_a_genotype = self.mutation_function(child_a_genotype)
+
+            if random.random() < self.mutation_rate:
+                child_b_genotype = self.mutation_function(child_b_genotype)
 
         return [child_a_genotype, child_b_genotype]
-
