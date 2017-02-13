66a5aa1a0aadd91aa0c6c4735fd7e5fb42026b15
Per Magnus Veierland <pveierland@gmail.com>
2017-02-13 11:09:15 +0100

    P2-2017: Updated grid search script

--- project_2_2017/scripts/grid_search.bash
+++ project_2_2017/scripts/grid_search.bash
@@ -2,11 +2,14 @@
 
 set -e
 
+instance="$1"
+
+crossover_randomness=0.1
 elitism_ratio=0.01
 evolve=100
-instance=7
+mutation_randomness=0.1
 tournament_randomness=0.1
-program="../program/mdvrp.py --script --instance ${instance} --evolve ${evolve} --tournament_randomness ${tournament_randomness} --elitism_ratio ${elitism_ratio}"
+program="../program/mdvrp.py --script --instance ${instance} --crossover_randomness ${crossover_randomness} --elitism_ratio ${elitism_ratio} --evolve ${evolve} --mutation_randomness ${mutation_randomness} --tournament_randomness ${tournament_randomness}"
 
 for population_size in 50 100 200
 do
@@ -14,11 +17,11 @@ do
     do
         for crossover_rate in 0.6 0.8 1.0
         do
-            for mutation_rate in 0.0 0.1 0.2
+            for mutation_rate in 0.0 0.1 0.2 0.5
             do
                 for run in {1..10}
                 do
-                    filename="../data/grid-search-result-instance-${instance}-evolve-${evolve}-population-${population_size}-tournament-group-${tournament_group_size}-tournament-randomness-${tournament_randomness}-crossover-${crossover_rate}-mutation-${mutation_rate}-run-${run}.txt"
+                    filename="../data/grid-search-result-instance-${instance}-evolve-${evolve}-population-${population_size}-tournament-group-${tournament_group_size}-crossover-${crossover_rate}-mutation-${mutation_rate}-run-${run}.txt"
                     cmd="${program} --population_size ${population_size} --tournament_group_size ${tournament_group_size} --crossover_rate ${crossover_rate} --mutation_rate ${mutation_rate}"
                     echo "${cmd}"
                     ${cmd} > ${filename} &
