56a214f5a83fc2a354a8ba549773cd672af81399
Per Magnus Veierland <pveierland@gmail.com>
2017-03-22 17:29:08 +0100

    P3-2017: Updated generation script

--- project_3_2017/scripts/generate_population_data.bash
+++ project_3_2017/scripts/generate_population_data.bash
@@ -12,32 +12,36 @@ mkdir -p ${data_path}/deviation_connectivity
 mkdir -p ${data_path}/deviation_edge
 mkdir -p ${data_path}/deviation_edge_connectivity
 
-eval ${program} --save_filename "${data_path}/edge_connectivity/population.txt" \
-                --evaluate_overall_deviation false \
-                --evaluate_edge_value true \
-                --evaluate_connectivity_measure true \
-                --render_base_path "${data_path}/edge_connectivity/" \
-                \"${image}\" &
+eval ${program} \"${image}\" \
+    --save_filename "${data_path}/edge_connectivity/population.txt" \
+    --evaluate_overall_deviation false \
+    --evaluate_edge_value true \
+    --evaluate_connectivity_measure true \
+    --render_base_path "${data_path}/edge_connectivity/" \
+    --hint 13 16 25 30 40 &
 
-eval ${program} --save_filename "${data_path}/deviation_connectivity/population.txt" \
-                --evaluate_overall_deviation true \
-                --evaluate_edge_value false \
-                --evaluate_connectivity_measure true \
-                --render_base_path "${data_path}/deviation_connectivity/" \
-                \"${image}\" &
+eval ${program} \"${image}\" \
+    --save_filename "${data_path}/deviation_connectivity/population.txt" \
+    --evaluate_overall_deviation true \
+    --evaluate_edge_value false \
+    --evaluate_connectivity_measure true \
+    --render_base_path "${data_path}/deviation_connectivity/" \
+    --hint 13 16 25 30 40 &
 
-eval ${program} --save_filename "${data_path}/deviation_edge/population.txt" \
-                --evaluate_overall_deviation true \
-                --evaluate_edge_value true \
-                --evaluate_connectivity_measure false \
-                --render_base_path "${data_path}/deviation_edge/" \
-                \"${image}\" &
+eval ${program} \"${image}\" \
+    --save_filename "${data_path}/deviation_edge/population.txt" \
+    --evaluate_overall_deviation true \
+    --evaluate_edge_value true \
+    --evaluate_connectivity_measure false \
+    --render_base_path "${data_path}/deviation_edge/" \
+    --hint 13 16 25 30 40 &
 
-eval ${program} --save_filename "${data_path}/deviation_edge_connectivity/population.txt" \
-                --evaluate_overall_deviation true \
-                --evaluate_edge_value true \
-                --evaluate_connectivity_measure true \
-                --render_base_path "${data_path}/deviation_edge_connectivity/" \
-                \"${image}\" &
+eval ${program} \"${image}\" \
+    --save_filename "${data_path}/deviation_edge_connectivity/population.txt" \
+    --evaluate_overall_deviation true \
+    --evaluate_edge_value true \
+    --evaluate_connectivity_measure true \
+    --render_base_path "${data_path}/deviation_edge_connectivity/" \
+    --hint 13 16 25 30 40 &
 
 wait
