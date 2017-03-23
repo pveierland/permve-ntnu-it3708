8d80514efb3e52c402c65b9ea22081df7b3a54c3
Per Magnus Veierland <pveierland@gmail.com>
2017-03-23 11:57:43 +0100

    P3-2017: Demo scripts

---
+++ project_3_2017/scripts/deviation_edge_connectivity.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+program="../../program/segmentation --generations 250 --population 200 --tournament_group_size 10 --save true"
+name="deviation_edge_connectivity"
+
+rm -rf "${name}"
+mkdir -p "${name}"
+cp "$1" "${name}/"
+cd "${name}"
+
+filename=$(basename "$1")
+mv "${filename}" "input.jpg"
+shift
+
+convert -resize 128x128^ "input.jpg" "input.jpg"
+
+eval ${program} "input.jpg" \
+    --save_filename "population.txt" \
+    --evaluate_overall_deviation true \
+    --evaluate_edge_value true \
+    --evaluate_connectivity_measure true \
+    --hint "$@"
+
+cp "../../report/demo_${name}.tex" .
+
+latexmk -f -pdf -interaction=nonstopmode -synctex=1 -latexoption=--shell-escape "demo_${name}.tex" > /dev/null 2>&1
