91da410f6234a1d690d0473a28f76658ef4e4ec6
Per Magnus Veierland <pveierland@gmail.com>
2017-03-28 21:43:59 +0200

    P4-2017: PSO WIP

--- project_4_2017/program/program.py
+++ project_4_2017/program/program.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 from collections import namedtuple
+import math
+import random
+import sys
+
+import numpy as np
 
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -9,8 +14,120 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtPrintSupport import *
 
 Allocation = namedtuple('Allocation', ['job_index', 'sequence_index', 'start_time', 'time_steps'])
-Operation  = namedtuple('Operation', ['machine_index', 'time_steps'])
+Operation  = namedtuple('Operation', ['job_index', 'machine_index', 'time_steps'])
 Problem    = namedtuple('Problem', ['job_count', 'machine_count', 'jobs'])
+
+class ParticleSwarmOptimizer(object):
+    Config = namedtuple('Config', [
+        'dimensions',
+        'population_size',
+        'low_value',
+        'high_value',
+        'max_velocity',
+        'omega',
+        'phi_particle',
+        'phi_global'])
+
+    def __init__(self, config, evaluator):
+        self.config    = config
+        self.evaluator = evaluator
+
+        self.individual_positions = np.random.uniform(
+            config.low_value, config.high_value, (config.population_size, config.dimensions))
+
+        self.individual_velocities = np.random.uniform(
+            -config.max_velocity, config.max_velocity, (config.population_size, config.dimensions))
+
+        self.individual_evaluations = np.zeros(config.population_size)
+
+        for i in range(self.config.population_size):
+            self.individual_evaluations[i] = self.evaluator(self.individual_positions[i])
+
+        self.individual_best_positions   = np.copy(self.individual_positions)
+        self.individual_best_evaluations = np.copy(self.individual_evaluations)
+
+        self.global_best_position   = self.individual_positions[0]
+        self.global_best_evaluation = self.individual_evaluations[0]
+
+        for i in range(1, self.config.population_size):
+            if self.individual_best_evaluations[i] < self.global_best_evaluation:
+                self.global_best_position   = self.individual_best_positions[i]
+                self.global_best_evaluation = self.individual_best_evaluations[i]
+
+    def evolve(self):
+        for i in range(1, self.config.population_size):
+            r_p = np.random.rand(self.config.dimensions)
+            r_g = np.random.rand(self.config.dimensions)
+
+            position = self.individual_positions[i]
+
+            velocity = np.clip(
+                self.config.omega * self.individual_velocities[i] +
+                self.config.phi_particle * r_p * (self.individual_best_positions[i] - position) +
+                self.config.phi_global   * r_g * (self.global_best_position - position),
+                -self.config.max_velocity,
+                +self.config.max_velocity)
+
+            position   = np.clip(position + velocity, self.config.low_value, self.config.high_value)
+            evaluation = self.evaluator(position)
+
+            self.individual_positions[i]   = position
+            self.individual_velocities[i]  = velocity
+            self.individual_evaluations[i] = evaluation
+
+            if evaluation < self.individual_best_evaluations[i]:
+                self.individual_best_positions[i]   = position
+                self.individual_best_evaluations[i] = evaluation
+
+                if evaluation < self.global_best_evaluation:
+                    self.global_best_position   = position
+                    self.global_best_evaluation = evaluation
+
+    def make_particle(self, position):
+        return Particle(position, self.evaluator(position))
+
+def allocate_operation_sequence(problem, operation_sequence):
+    job_completion_times     = np.zeros(problem.job_count)
+    machine_completion_times = np.zeros(problem.machine_count)
+    sequence_indexes         = np.zeros(problem.job_count, int)
+
+    allocations = [[] for _ in range(problem.machine_count)]
+
+    for operation in operation_sequence:
+        operation_start_time = max(job_completion_times[operation.job_index],
+                                   machine_completion_times[operation.machine_index])
+
+        operation_completion_time = operation_start_time + operation.time_steps
+
+        job_completion_times[operation.job_index]         = operation_completion_time
+        machine_completion_times[operation.machine_index] = operation_completion_time
+
+        allocations[operation.machine_index].append(
+            Allocation(operation.job_index,
+                       sequence_indexes[operation.job_index],
+                       operation_start_time,
+                       operation.time_steps))
+
+        sequence_indexes[operation.job_index] += 1
+
+    return allocations, max(machine_completion_times)
+
+def decode_random_key_operation_sequence(problem, random_key):
+    ranks = np.empty(problem.job_count * problem.machine_count, int)
+    ranks[random_key.argsort()] = np.arange(problem.job_count * problem.machine_count)
+
+    job_indexes        = ranks % problem.job_count
+    sequence_indexes   = np.zeros(problem.job_count, int)
+    operation_sequence = []
+
+    for job_index in job_indexes:
+        sequence_index = sequence_indexes[job_index]
+        sequence_indexes[job_index] += 1
+
+        operation = problem.jobs[job_index][sequence_index]
+        operation_sequence.append(operation)
+
+    return operation_sequence
 
 def parse_problem_file(filename):
     with open(filename) as input_file:
@@ -20,7 +137,7 @@ def parse_problem_file(filename):
 
         for job_index in range(job_count):
             input_line = list(map(int, input_lines[job_index + 1].split()))
-            jobs.append([Operation(machine_index, time_steps)
+            jobs.append([Operation(job_index, machine_index, time_steps)
                          for machine_index, time_steps in zip(input_line[0::2], input_line[1::2])])
 
         return Problem(job_count, machine_count, jobs)
@@ -61,10 +178,13 @@ def render_gantt_chart(output_filename, allocations):
     job_colors = [QColor(c) for c in ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f']]
 
     num_machines = len(allocations)
-    width        = max(allocation.start_time + allocation.time_steps
-                       for machine_allocations in allocations
-                       for allocation in machine_allocations)
-    height       = num_machines * configuration['cell_height']
+
+    max_time_step = max(allocation.start_time + allocation.time_steps
+                        for machine_allocations in allocations
+                        for allocation in machine_allocations)
+
+    width  = int(math.ceil(max_time_step / configuration['major_grid_resolution']) * configuration['major_grid_resolution'])
+    height = num_machines * configuration['cell_height']
 
     app = QApplication([ '-platform', 'offscreen'])
 
@@ -157,5 +277,29 @@ def render_gantt_chart(output_filename, allocations):
 
     painter.end()
 
-#render_gantt_chart('wtf.pdf', None)
-print(parse_problem_file('/home/pveierland/permve-ntnu-it3708/project_4_2017/assignment/Test Data/1.txt'))
+problem = parse_problem_file(sys.argv[1])
+
+#rk = np.random.rand(problem.job_count * problem.machine_count)
+#rk = np.array([1.3, 0.7, 2.4, 1.1, 3.4, 5.3])
+#operation_sequence = decode_random_key_operation_sequence(problem, rk)
+#allocations, makespan = allocate_operation_sequence(problem, operation_sequence)
+#print(makespan)
+#render_gantt_chart('wtf.pdf', allocations)
+
+
+pso = ParticleSwarmOptimizer(
+    ParticleSwarmOptimizer.Config(
+        problem.job_count * problem.machine_count,
+        1000,
+        1.0,
+        float(problem.job_count * problem.machine_count),
+        0.1 * float(problem.job_count * problem.machine_count),
+        0.9,
+        2.0,
+        2.0),
+    (lambda rk: allocate_operation_sequence(
+        problem, decode_random_key_operation_sequence(problem, rk))[1]))
+
+for _ in range(10000):
+    pso.evolve()
+    print(pso.global_best_evaluation)
