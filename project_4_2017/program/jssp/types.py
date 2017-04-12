5e9f68d42d686f7cf0531d71e05c2851ac27ffcf
Per Magnus Veierland <pveierland@gmail.com>
2017-04-12 21:59:42 +0200

    P4-2017: JSSP problem fixed operation index + added lookup

--- project_4_2017/program/jssp/types.py
+++ project_4_2017/program/jssp/types.py
@@ -2,6 +2,6 @@ from collections import namedtuple
 
 Allocation = namedtuple('Allocation', ['job_sequence_index', 'machine_sequence_index', 'start_time', 'job_predecessor', 'machine_predecessor', 'operation'])
 Operation  = namedtuple('Operation', ['index', 'job', 'machine', 'job_sequence_index', 'time_steps'])
-Problem    = namedtuple('Problem', ['job_count', 'machine_count', 'jobs'])
+Problem    = namedtuple('Problem', ['job_count', 'machine_count', 'jobs', 'operations'])
 Solution   = namedtuple('Solution', ['operations', 'makespan'])
 Reorder    = namedtuple('Reorder', ['machine', 'machine_sequence_edge', 'machine_sequence_index', 'operation', 'after'])
